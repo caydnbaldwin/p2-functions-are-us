132a8f4e9b756be11f4fd3523de4ec126a4593d6
caydnbaldwin <caydnbaldwin@gmail.com>
2025-02-27 13:26:51 -0700

    Added structure and logical flow to .

--- functions_are_us.py
+++ functions_are_us.py
@@ -1,9 +1,32 @@
-# write all functions below this line, but above the main function
+# Aaron McCoy, Anders Houghton, Caydn Baldwin, George Martinez, Ryan Bartholomew, Waylan Abbott
+# IS 303 Section 4
+# P2 - Functions Are Us
+# A program that plays the women’s soccer season as defined in Assignment 4.
+
+import _ as function_1
+import _ as function_2
+import _ as function_3
+import _ as function_4
+import _ as function_5
 
 # main function, logic begins here, all functions should be above this line
 def main():
+    # 1
+        # Display an introduction to the game explaining rules and prompt for their name and display that in the welcome message. Return the name to the main program and store it in variable so it can be used throughout the program.
+    pass
+    # 2
+        # Display of menu and return choice. Store in variable and use this value to determine which function to call next.
+    pass
+    # 3
+        # Display list of all teams and allow the user to choose a team using a menu. Call the function again to let the user choose the opponent but do not display the team they chose previously. Remove that team from the list. Allow the user to select an opponent, and return team name. This function should receive a parameter but give it a default value if none is passed. You can use this function for both choosing the home team and the opponent team.
+    pass
+    # 4
+        # Play the game receiving both team names. Generate random scores without ties. Return W or L.
+    pass
+    # 5
+        # Display the final record for a team. Receive the home team data and display information.
     pass
 
-# check if script is run directly as the main program
+# check if script is run directly as the main program, then execute `main()`
 if __name__ == "__main__":
     main()
